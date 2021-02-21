0a3d9347b912659b5d79f99c8338b4d4c1b65935
unknown <you@example.com>
2021-02-21 08:43:06 -0800

    committing arrays equals, compare, mismatch

---
+++ Hello_18.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Hello_18 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int [] arr1 = {1,2,3,4,5};
+		int [] arr2 = arr1;
+		int [] arr3 = {7,8,9};
+		int [] arr4 = {1,2};
+		int [] arr5 = {1,2,3,4,5};
+		
+		System.out.println(arr1.equals(arr5));
+		System.out.println(Arrays.compare(arr1,arr2));
+		System.out.println(Arrays.compare(arr1,arr3));
+		System.out.println(Arrays.mismatch(arr1,arr4));
+		
+		
+
+	}
+
+}
